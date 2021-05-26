9de9a6960a3a12a7cffef3f8b4905d83f6d579c7
Adetola <ybdtola1@gmail.com>
2021-05-26 13:22:58 +0100

    Portfolio

--- js/balls.js
+++ js/balls.js
@@ -16,7 +16,7 @@ Pts.quickStart( "#pt_container", "#ffffff00" );
       
       // Create particles and hit them with a random impulse
       for (let i=0, len=pts.length; i<len; i++) {
-        let p = new Particle( pts[i] ).size( (i===0) ? 20 : 20);
+        let p = new Particle( pts[i] ).size( (i===0) ? 25 : 20);
         p.hit( Num.randomRange(-50,50), Num.randomRange(-25, 25) );
         world.add( p );
       }
